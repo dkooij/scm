7a04fa6e35f981ca13cd204b85b43c663d1cf196
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-06-04 11:22:57 +0200

    Adapt archiver to securely generate Kerberos tokens

--- crawler/archiver.py
+++ crawler/archiver.py
@@ -1,17 +1,21 @@
 """
-Web Crawler.
+Crawl result archiver.
+Moves crawl results from local file system to DFS.
 Author: Daan Kooij
-Last modified: May 19th, 2021
+Last modified: June 4th, 2021
 """
 
 import os
 
 
+KEYTAB_FILE = "/home/s1839047/keytabs/username.keytab"
+KEYTAB_USER = "s1839047@AD.UTWENTE.NL"
 CRAWL_DIR = "output"
 DFS_DIR = "/user/s1839047/crawls"
 DONE_FILE = "done.txt"
 
 
+os.system("kinit -kt " + KEYTAB_FILE + " " + KEYTAB_USER)
 for crawl_name in os.listdir(CRAWL_DIR):
     crawl_path = CRAWL_DIR + "/" + crawl_name
     if os.path.isdir(crawl_path):
