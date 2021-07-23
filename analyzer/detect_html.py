97e8073e4cc9f252214ac259758e317060978363
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-07-23 09:31:30 +0200

    Clean up HTML detector

--- analyzer/detect_html.py
+++ analyzer/detect_html.py
@@ -5,21 +5,6 @@ Last modified: July 23rd, 2021
 """
 
 from bs4 import BeautifulSoup
-import os
-
-
-INPUT_DIR = "input"
-
-
-for filename in sorted(os.listdir(INPUT_DIR)):
-    filepath = INPUT_DIR + "/" + filename
-    try:
-        with open(filepath) as file:
-            x = bool(BeautifulSoup(file, "html.parser").find())
-            if not x:
-                print("no html " + filepath)
-    except UnicodeDecodeError:
-        print("error with " + filepath)
 
 
 def is_html(file):
