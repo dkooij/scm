c10a23bb49ac41c8e058cb8a019773b03e1f4131
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-07-23 11:57:23 +0200

    Update data point print representation

--- analyzer/data_point.py
+++ analyzer/data_point.py
@@ -8,10 +8,13 @@ Last modified: July 23rd, 2021
 class DataPoint:
 
     def __init__(self):
-        self._features = dict()
+        self.features = dict()
 
     def set_feature(self, feature_name, feature_value):
-        self._features[feature_name] = feature_value
+        self.features[feature_name] = feature_value
 
     def get_feature(self, feature_name):
-        return self._features[feature_name]
+        return self.features[feature_name]
+
+    def __repr__(self):
+        return str(self.features)
