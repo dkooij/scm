102ece284d9266ccf5d14c624142a2d0884bbcd2
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-07-23 11:58:38 +0200

    Create feature extractor

---
+++ analyzer/extractor.py
@@ -0,0 +1,64 @@
+"""
+Feature Extractor.
+Author: Daan Kooij
+Last modified: July 23rd, 2021
+"""
+
+from datetime import datetime
+import os
+
+import csv_reader
+from data_point import DataPoint
+import detect_html
+
+
+INPUT_DIR = "input"
+
+
+# Control functions
+
+def get_data_points():
+    data_points = []
+
+    for log_filename in os.listdir(INPUT_DIR):
+        log_path = INPUT_DIR + "/" + log_filename
+
+        if csv_reader.is_csv(log_path):
+            for log_entry in csv_reader.read_csv(log_path):
+                if should_use_page(log_entry):
+                    with open(get_filepath(log_entry)) as raw_page:
+                        if detect_html.is_html(raw_page):
+                            data_point = extract_features(log_entry, raw_page)
+                            data_points.append(data_point)
+
+    return data_points
+
+
+def extract_features(log_entry, raw_page):
+    data_point = DataPoint()
+
+    return data_point
+
+
+# Log entry functions
+
+def should_use_page(log_entry):
+    return log_entry["File present"] == "True" and \
+           log_entry["Status code"] == "RequestStatus.HEADLESS_SUCCESS"
+
+
+def get_filepath(log_entry):
+    return INPUT_DIR + "/pages/" + log_entry["Stage file"] + "-" + log_entry["URL index"]
+
+
+def get_timestamp(log_entry):
+    timestamp_str = log_entry["Timestamp"]
+    return datetime.strptime(timestamp_str, "%Y%m%d%H%M%S")
+
+
+def get_url(log_entry):
+    return log_entry["URL"]
+
+
+# Invoke base function
+dps = get_data_points()
