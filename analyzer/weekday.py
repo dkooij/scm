1b54b8afb520c548c69c679d154f1debf6ebda85
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-07-23 11:55:57 +0200

    Add Weekday enum

---
+++ analyzer/weekday.py
@@ -0,0 +1,17 @@
+"""
+Weekday enum.
+Author: Daan Kooij
+Last modified: July 23rd, 2021
+"""
+
+from enum import Enum
+
+
+class Weekday(Enum):
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
