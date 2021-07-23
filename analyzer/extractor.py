8c46d767cfe8a57359ff2799defeceb116fd1671
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-07-23 12:01:15 +0200

    Add extraction of URL features

--- analyzer/extractor.py
+++ analyzer/extractor.py
@@ -7,6 +7,7 @@ Last modified: July 23rd, 2021
 from datetime import datetime
 import os
 
+from analyzer.protocol import Protocol
 from analyzer.weekday import Weekday
 import csv_reader
 from data_point import DataPoint
@@ -40,6 +41,13 @@ def extract_features(log_entry, raw_page):
 
     data_point.set_feature("size", get_file_size(log_entry))
     data_point.set_feature("weekday", get_weekday(log_entry))
+    data_point.set_feature("url_length", get_url_length(log_entry))
+    data_point.set_feature("domain_name_length", get_domain_name_length(log_entry))
+    data_point.set_feature("domain_name_digits", get_domain_name_digits(log_entry))
+    data_point.set_feature("domain_name_special_chars", get_domain_name_special_chars(log_entry))
+    data_point.set_feature("url_subdir_depth", get_url_subdir_depth(log_entry))
+    data_point.set_feature("url_subdomain_depth", get_url_subdomain_depth(log_entry))
+    data_point.set_feature("protocol", get_protocol(log_entry))
 
     return data_point
 
@@ -64,6 +72,25 @@ def get_url(log_entry):
     return log_entry["URL"]
 
 
+# URL helper functions
+
+def get_sl_domain(url):
+    url_after_protocol = url.split("://")[1]
+    url_before_subdirs = url_after_protocol.split("/", 1)[0]
+    return url_before_subdirs.rsplit(".", 2)[-2]
+
+
+def get_url_subdirs(url):
+    url_after_protocol = url.split("://")[1]
+    return url_after_protocol.split("/")[1:]
+
+
+def get_url_subdomains(url):
+    url_after_protocol = url.split("://")[1]
+    url_before_subdirs = url_after_protocol.split("/", 1)[0]
+    return url_before_subdirs.split(".")[:-2]
+
+
 # Meta feature extraction functions
 
 def get_file_size(log_entry):
@@ -75,6 +102,52 @@ def get_weekday(log_entry):
     timestamp = get_timestamp(log_entry)
     weekday_int = timestamp.weekday()
     return Weekday(weekday_int)
+
+
+# URL feature extraction functions
+
+def get_url_length(log_entry):
+    url = get_url(log_entry)
+    return len(url)
+
+
+def get_domain_name_length(log_entry):
+    url = get_url(log_entry)
+    sl_domain = get_sl_domain(url)
+    return len(sl_domain)
+
+
+def get_domain_name_digits(log_entry):
+    url = get_url(log_entry)
+    sl_domain = get_sl_domain(url)
+    return sum(c.isdigit() for c in sl_domain)
+
+
+def get_domain_name_special_chars(log_entry):
+    url = get_url(log_entry)
+    sl_domain = get_sl_domain(url)
+    return sum(not (c.isdigit() or c.isalpha()) for c in sl_domain)
+
+
+def get_url_subdir_depth(log_entry):
+    url = get_url(log_entry)
+    subdirs = get_url_subdirs(url)
+    return len(subdirs)
+
+
+def get_url_subdomain_depth(log_entry):
+    url = get_url(log_entry)
+    subdomains = get_url_subdomains(url)
+    return len(subdomains)
+
+
+def get_protocol(log_entry):
+    url = get_url(log_entry)
+    protocol_str = url.split("://")[0]
+    if protocol_str.lower() == "https":
+        return Protocol.HTTPS
+    else:
+        return Protocol.HTTP
 
 
 # Invoke base function
