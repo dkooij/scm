7b6ff56727bc22478488090000a6154d549f0503
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-07-27 08:55:47 +0200

    Move completed crawls to crawl backlog in case HDFS is unreachable

--- crawler/archiver.py
+++ crawler/archiver.py
@@ -2,7 +2,7 @@
 Crawl result archiver.
 Moves crawl results from local file system to DFS.
 Author: Daan Kooij
-Last modified: June 4th, 2021
+Last modified: July 27th, 2021
 """
 
 import os
@@ -11,6 +11,7 @@ import os
 KEYTAB_FILE = "/home/s1839047/keytabs/username.keytab"
 KEYTAB_USER = "s1839047@AD.UTWENTE.NL"
 CRAWL_DIR = "output"
+CRAWL_BACKLOG_DIR = "output_backlog"
 DFS_DIR = "/user/s1839047/crawls"
 DONE_FILE = "done.txt"
 
@@ -18,6 +19,10 @@ DONE_FILE = "done.txt"
 os.system("kinit -kt " + KEYTAB_FILE + " " + KEYTAB_USER)
 for crawl_name in os.listdir(CRAWL_DIR):
     crawl_path = CRAWL_DIR + "/" + crawl_name
-    if os.path.isdir(crawl_path):
-        if os.path.exists(crawl_path + "/" + DONE_FILE):
-            os.system("hdfs dfs -moveFromLocal " + crawl_path + " " + DFS_DIR + "/" + crawl_name)
+    if os.path.isdir(crawl_path) and os.path.exists(crawl_path + "/" + DONE_FILE):
+        os.system("hdfs dfs -moveFromLocal " + crawl_path + " " + DFS_DIR + "/" + crawl_name)
+
+    # If the move to HDFS was unsuccessful, move the completed crawl to the crawl backlog
+    if os.path.isdir(crawl_path) and os.path.exists(crawl_path + "/" + DONE_FILE):
+        crawl_backlog_path = CRAWL_BACKLOG_DIR + "/" + crawl_name
+        os.system("mv " + crawl_path + " " + crawl_backlog_path)
