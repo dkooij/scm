a36112cebcf0405dc0bd8d2dbe38510e52743b00
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-08-05 11:40:13 +0200

    Apply PCA to real embedding tensors

--- analyzer/pca.py
+++ analyzer/pca.py
@@ -1,6 +1,37 @@
+"""
+Reduce dimensionality of data by performing Principal Component Analysis (PCA).
+Author: Daan Kooij
+Last modified: August 5th, 2021
+"""
+
+import os
 from sklearn.decomposition import PCA
+import torch
 
 
+INPUT_DIR = "tensor"
+
+
+def load_embeddings():
+    embeddings = []
+
+    for tensor_filename in os.listdir(INPUT_DIR):
+        tensor_path = INPUT_DIR + "/" + tensor_filename
+        tensor = torch.load(tensor_path)
+        embeddings.append(tensor.numpy())
+
+    return embeddings
+
+
+embeddings = load_embeddings()
+pca = PCA(n_components=24)
+pca.fit(embeddings)
+
+principal_components = pca.transform(embeddings)
+projections = pca.inverse_transform(principal_components)
+print(sum(pca.explained_variance_ratio_))
+
+"""
 data = [[1, 0, 2, 0],
         [0, 4, 0, 3],
         [4, 0, 8, 0],
@@ -13,3 +44,4 @@ data = [[1, 0, 2, 0],
 pca = PCA(n_components=2)
 principal_components = pca.fit_transform(data)
 original = pca.inverse_transform(principal_components)
+"""
