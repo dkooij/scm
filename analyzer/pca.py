b80f7c225882478eadcb3dc82e7adee3b5923d32
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-08-05 12:03:12 +0200

    Allow PCA to be trained with subset of embeddings

--- analyzer/pca.py
+++ analyzer/pca.py
@@ -4,18 +4,23 @@ Author: Daan Kooij
 Last modified: August 5th, 2021
 """
 
+import itertools
 import os
+import pickle
 from sklearn.decomposition import PCA
 import torch
 
 
 INPUT_DIR = "tensor"
+OUTPUT_FILE = "model/pca.skl"
 
 
-def load_embeddings():
+def load_embeddings(indices=None):
     embeddings = []
 
-    for tensor_filename in os.listdir(INPUT_DIR):
+    for i, tensor_filename in zip(itertools.count(), os.listdir(INPUT_DIR)):
+        if indices is not None and i not in indices:
+            continue  # Do not use this tensor for training the PCA
         tensor_path = INPUT_DIR + "/" + tensor_filename
         tensor = torch.load(tensor_path)
         embeddings.append(tensor.numpy())
@@ -23,25 +28,12 @@ def load_embeddings():
     return embeddings
 
 
-embeddings = load_embeddings()
-pca = PCA(n_components=24)
-pca.fit(embeddings)
+def train_pca(n_components=24, indices=None):
+    pca = PCA(n_components=n_components)
+    embeddings = load_embeddings(indices=indices)
+    pca.fit(embeddings)
 
-principal_components = pca.transform(embeddings)
-projections = pca.inverse_transform(principal_components)
-print(sum(pca.explained_variance_ratio_))
+    return pca
 
-"""
-data = [[1, 0, 2, 0],
-        [0, 4, 0, 3],
-        [4, 0, 8, 0],
-        [0, 1, 0, 0],
-        [2, 3, 4, 2],
-        [3, 2, 6, 1],
-        [1, 1, 2, 0],
-        [4, 2, 8, 1]]
 
-pca = PCA(n_components=2)
-principal_components = pca.fit_transform(data)
-original = pca.inverse_transform(principal_components)
-"""
+train_pca()
