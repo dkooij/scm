2ea852b5201987f3fa98e00bd308bfc8e3920080
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-08-31 15:02:46 +0200

    Only use content of pages containing html tags

--- analyzer/detect_html.py
+++ analyzer/detect_html.py
@@ -10,6 +10,6 @@ from bs4 import BeautifulSoup
 def get_html(file):
     # Requires file in binary-reading mode
     page_html = BeautifulSoup(file, "html.parser")
-    if page_html.find():
+    if page_html.find("html"):
         return page_html
     return False
