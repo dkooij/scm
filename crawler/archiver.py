09e7a3a6c74b7df79ca3f93431c82e2dc448cca0
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-09-02 11:06:57 +0200

    Add support for archiving aborted/incomplete crawls

--- crawler/archiver.py
+++ crawler/archiver.py
@@ -2,7 +2,7 @@
 Crawl result archiver.
 Moves crawl results from local file system to DFS.
 Author: Daan Kooij
-Last modified: July 27th, 2021
+Last modified: September 2nd, 2021
 """
 
 import os
@@ -14,12 +14,14 @@ CRAWL_DIR = "output"
 CRAWL_BACKLOG_DIR = "output_backlog"
 DFS_DIR = "/user/s1839047/crawls"
 DONE_FILE = "done.txt"
+INCOMPLETE_FILE = "incomplete.txt"
 
 
 os.system("kinit -kt " + KEYTAB_FILE + " " + KEYTAB_USER)
 for crawl_name in os.listdir(CRAWL_DIR):
     crawl_path = CRAWL_DIR + "/" + crawl_name
-    if os.path.isdir(crawl_path) and os.path.exists(crawl_path + "/" + DONE_FILE):
+    if os.path.isdir(crawl_path) and \
+            (os.path.exists(crawl_path + "/" + DONE_FILE) or os.path.exists(crawl_path + "/" + INCOMPLETE_FILE)):
         os.system("hdfs dfs -moveFromLocal " + crawl_path + " " + DFS_DIR + "/" + crawl_name)
 
     # If the move to HDFS was unsuccessful, move the completed crawl to the crawl backlog
