d1494b862aba2cfd50f5631b9fc19bad2eacb7b8
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-09-08 14:27:13 +0200

    Enforce passing input directory as argument in CSV reader

--- analyzer/csv_reader.py
+++ analyzer/csv_reader.py
@@ -9,9 +9,6 @@ import csv
 import os
 
 
-INPUT_DIR = "input"
-
-
 # Generic CSV reader functions
 
 def is_csv(filepath):
@@ -28,10 +25,10 @@ def read_csv(filepath):
 
 # Crawler-specific CSV reader functions
 
-def get_log_paths():
+def get_log_paths(input_dir):
     log_paths = []
-    for log_filename in os.listdir(INPUT_DIR):
-        log_path = INPUT_DIR + "/" + log_filename
+    for log_filename in os.listdir(input_dir):
+        log_path = input_dir + "/" + log_filename
         if is_csv(log_path):
             log_paths.append(log_path)
     return log_paths
@@ -43,8 +40,8 @@ def get_log_entries(log_path):
             yield log_entry
 
 
-def get_all_log_entries():
-    for log_path in get_log_paths():
+def get_all_log_entries(input_dir):
+    for log_path in get_log_paths(input_dir):
         for log_entry in get_log_entries(log_path):
             yield log_entry
 
@@ -53,8 +50,8 @@ def get_filename(log_entry):
     return log_entry["Stage file"] + "-" + log_entry["URL index"]
 
 
-def get_filepath(log_entry):
-    return INPUT_DIR + "/pages/" + get_filename(log_entry)
+def get_filepath(log_entry, input_dir):
+    return input_dir + "/pages/" + get_filename(log_entry)
 
 
 def should_use_page(log_entry):
