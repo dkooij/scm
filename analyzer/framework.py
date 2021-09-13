2e00c4ac4e18475fa6c747d9e3110d4dd7c424b1
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-09-13 12:34:53 +0200

    Finalize framework text extract step

--- analyzer/framework.py
+++ analyzer/framework.py
@@ -2,9 +2,8 @@
 Data pre-processing framework.
 Takes a collection of web crawls, and converts it to pre-processed data points.
 Author: Daan Kooij
-Last modified: September 8th, 2021
+Last modified: September 13th, 2021
 """
-
 
 import csv
 import itertools
@@ -13,7 +12,6 @@ import os
 
 import csv_reader
 import detect_html
-from embedding import get_page_text
 
 
 BATCH_SIZE = 1000
@@ -22,11 +20,10 @@ EXTRACT_ROOT = "D:/extracted"
 PAGE_TEXT_DIR = "text"
 
 
-
 target_list = ["20210612"]
 
 
-def extract_page_text(log_path, crawl_dir, output_filepath):
+def _extract_page_text(log_path, crawl_dir, output_filepath):
     with open(output_filepath, "w", newline="") as output_file:
         output_writer = csv.writer(output_file)
         output_writer.writerow(["Stage file", "URL index", "URL", "Page text"])
@@ -34,13 +31,15 @@ def extract_page_text(log_path, crawl_dir, output_filepath):
         for log_entry in csv_reader.get_log_entries(log_path):
             with open(csv_reader.get_filepath(log_entry, crawl_dir), "rb") as file:
                 page_html = detect_html.get_html(file)
-                page_text = get_page_text(page_html)
-                output_writer.writerow([log_entry["Stage file"], log_entry["URL index"], log_entry["URL"], page_text])
+                if page_html:
+                    page_text = detect_html.get_page_text(page_html)
+                    output_writer.writerow([log_entry["Stage file"], log_entry["URL index"],
+                                            log_entry["URL"], page_text])
 
     print(log_path + " done")
 
 
-def fun():
+def extract_page_text():
     for target in target_list:
         output_dir = EXTRACT_ROOT + "/" + target + "/" + PAGE_TEXT_DIR
         os.makedirs(output_dir, exist_ok=True)
@@ -48,9 +47,8 @@ def fun():
         crawl_dir = CRAWLS_ROOT + "/" + target
         for tid, log_path in zip(itertools.count(), csv_reader.get_log_paths(crawl_dir)):
             output_filepath = output_dir + "/" + "text-" + str(tid) + ".csv"
-            # extract_page_text(log_path, crawl_dir, output_filepath)
-            Process(target=extract_page_text, args=(log_path, crawl_dir, output_filepath)).start()
+            Process(target=_extract_page_text, args=(log_path, crawl_dir, output_filepath)).start()
 
 
 if __name__ == "__main__":
-    fun()
+    extract_page_text()
