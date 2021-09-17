375871342f9f4d5c64cbc4054c55779f25d5d46f
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-09-17 14:49:27 +0200

    Read pages from HDFS as RDD (proof of concept)

---
+++ analyzer/cluster/read_pages_as_rdd.py
@@ -0,0 +1,30 @@
+"""
+Read pages from HDFS as Resilient Distributed Dataset.
+Author: Daan Kooij
+Last modified: September 17th, 2021
+"""
+
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+
+# Initialize Spark and SparkSQL context.
+sc = SparkContext(appName="SPARKTEST-S1839047")
+sc.setLogLevel("ERROR")
+spark = SparkSession.builder.getOrCreate()
+
+
+def get_html_features(binary):
+    import allow_super_imports
+    from data_point import DataPoint
+    import detect_html
+    import extractor
+
+    page_html = detect_html.get_html(binary)
+    data_point = DataPoint()
+    extractor.set_html_features(page_html, data_point)
+    return [v for k, v in sorted(data_point.features.items())]
+
+
+rdd = sc.binaryFiles("/user/s1839047/sparktest/testday/pages")
+rdd2 = rdd.map(lambda x: ([x[0]] + get_html_features(x[1])))
+print(rdd2.take(5))
