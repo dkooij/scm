555bfbb933075249bd82346e8b1e2305754dff70
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-09-28 11:40:22 +0200

    Join crawl log entries and binary data

--- analyzer/read_pages_as_rdd.py
+++ analyzer/read_pages_as_rdd.py
@@ -4,7 +4,7 @@ Author: Daan Kooij
 Last modified: September 28th, 2021
 """
 
-from pyspark import SparkContext
+from pyspark import SparkContext, Row
 from pyspark.sql import SparkSession
 
 from data_point import DataPoint
@@ -18,6 +18,9 @@ sc.setLogLevel("ERROR")
 spark = SparkSession.builder.getOrCreate()
 
 
+BinaryTriple = Row("Stage file", "URL index", "Binary data")
+
+
 def get_html_features(binary):
     page_html = detect_html.get_html(binary)
     data_point = DataPoint()
@@ -25,6 +28,26 @@ def get_html_features(binary):
     return [v for k, v in sorted(data_point.features.items())]
 
 
-rdd = sc.binaryFiles("/user/s1839047/sparktest/testday/pages")
-rdd2 = rdd.map(lambda x: ([x[0]] + get_html_features(x[1])))
-print(rdd2.take(5))
+def process_binary_tuple(binary_tuple):
+    # Converts (file_path, binary_data)-tuple to
+    # (stage_file, url_index, binary_data)-triple.
+    (file_path, binary_data) = binary_tuple
+    file_name = file_path.split("/")[-1]
+    [stage_file, url_index] = file_name.split("-")
+    return BinaryTriple(stage_file, url_index, bytearray(binary_data))
+
+
+def crawl_to_rdd(crawl_directory):
+    df_log_entries = spark.read.csv(crawl_directory + "/*.csv", header=True)
+    df_binary_files = sc.binaryFiles(crawl_directory + "/pages").map(process_binary_tuple).toDF()
+
+    combined_df = df_log_entries.join(df_binary_files, ["Stage file", "URL index"])
+    print(combined_df.count())
+    print(combined_df.take(2))
+
+
+crawl_to_rdd("/user/s1839047/sparktest/testday")
+
+
+# rdd2 = rdd_binary_files.map(lambda x: ([x[0]] + get_html_features(x[1])))
+# print(rdd2.take(5))
