f2f85d3ac328cbcdffadce9554c756b117cd7dd9
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-10-08 16:21:46 +0200

    Update page text extractor to produce prettier output

--- analyzer/get_page_text.py
+++ analyzer/get_page_text.py
@@ -4,12 +4,22 @@ Author: Daan Kooij
 Last modified: October 8th, 2021
 """
 
-from html2text import html2text
+from html2text import HTML2Text
 
 
 def get_page_text(binary_data):
     try:
-        page_data_str = str(binary_data)
-        return html2text(page_data_str)
+        page_data_str = str(binary_data, encoding="utf-8")
+        parser = HTML2Text()
+        parser.ignore_links = True
+        parser.ignore_emphasis = True
+        parser.ignore_images = True
+        parser.ignore_tables = True
+        page_text = parser.handle(page_data_str)
+        words = page_text.strip().split()
+        return " ".join(words)
     except UnicodeDecodeError:
-        return ""
+        print("except UnicodeDecodeError")
+    except NotImplementedError:
+        print("except NotImplementedError")
+    return ""
