d51f947245eb5ebd9278736024d099c9d4810784
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-11-11 15:58:33 +0100

    Add function to compute change statistics (equal, added, removed)-elements

--- analyzer/text_overlap.py
+++ analyzer/text_overlap.py
@@ -10,28 +10,53 @@ import difflib
 import csv_reader
 
 
-def get_overlap_fraction(text1, text2):
-    return difflib.SequenceMatcher(None, text1, text2).ratio()
+def get_overlap_fraction(value1_list, value2_list):
+    return difflib.SequenceMatcher(None, value1_list, value2_list).ratio()
 
 
-def start_overlap(text1, text2, start_length=3):
-    return text1[:start_length] == text2[:start_length]
+def start_overlap(value1_list, value2_list, start_length=3):
+    return value1_list[:start_length] == value2_list[:start_length]
 
 
-def compute_change(input1_dir, input2_dir, output_dir):
-    entry_path1 = output_dir + "/" + input1_dir + "/combined/text.csv"
-    entry_path2 = output_dir + "/" + input2_dir + "/combined/text.csv"
+def get_opcode_stats(value1_list, value2_list):
+    op_list = difflib.SequenceMatcher(None, value1_list, value2_list).get_opcodes()
+    equal, insert, delete = 0, 0, 0
+    for (tag, i1, i2, j1, j2) in op_list:
+        if tag == "equal":
+            equal += i2 - i1
+        elif tag == "insert":
+            insert += j2 - j1
+        elif tag == "delete":
+            delete += i2 - i1
+        elif tag == "replace":
+            insert += j2 - j1
+            delete += i2 - i1
+    return equal, insert, delete
+
+
+def compute_change(input1_dir, input2_dir, output_dir, name, include_equal=True):
+    entry_path1 = output_dir + "/" + input1_dir + "/combined/" + name + ".csv"
+    entry_path2 = output_dir + "/" + input2_dir + "/combined/" + name + ".csv"
+    equal, insert, delete = 0, 0, 0
     for log_entry_pair in csv_reader.get_log_entry_pairs(entry_path1, entry_path2, ignore_validity_check=True):
-        text1_list, text2_list = log_entry_pair["text-1"], log_entry_pair["text-2"]
-        text1, text2 = ast.literal_eval(text1_list), ast.literal_eval(text2_list)
-        if text1 != text2:
-            h = difflib.HtmlDiff()
-            d = h.make_file(text1, text2)
-            with open("test.html", "w") as file:
-                file.write(d)
-                print()
+        value1_str_list, value2_str_list = log_entry_pair[name + "-1"], log_entry_pair[name + "-2"]
+        value1_list, value2_list = ast.literal_eval(value1_str_list), ast.literal_eval(value2_str_list)
+        t = get_opcode_stats(value1_list, value2_list)
+        if include_equal or t[1] != 0 or t[2] != 0:
+            equal += t[0]
+            insert += t[1]
+            delete += t[2]
+    return equal, insert, delete
+
+        # if text1 != text2:
+        #     h = difflib.HtmlDiff()
+        #     d = h.make_file(text1, text2)
+        #     with open("test.html", "w") as file:
+        #         file.write(d)
+        #         print()
             # x = get_overlap_fraction(text1, text2)
             # print(x)
 
 
-# compute_change("testminiday", "testminiday2", "outputmini")
+# print(compute_change("testminiday", "testminiday2", "outputmini", "text"))
+# print(compute_change("testminiday", "testminiday2", "outputmini", "text", include_equal=False))
