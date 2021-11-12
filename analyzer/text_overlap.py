d80f54ac6c1d66994e3bd788d5f2c2627225a0d1
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-11-12 12:07:09 +0100

    Add sampling option to HTML difference comparison

--- analyzer/text_overlap.py
+++ analyzer/text_overlap.py
@@ -7,6 +7,7 @@ Last modified: November 12th, 2021
 import ast
 import difflib
 import os
+import random
 
 import csv_reader
 
@@ -50,12 +51,23 @@ def compute_change(input1_dir, input2_dir, output_dir, name, include_equal=True)
     return equal, insert, delete
 
 
-def html_compare(input1_dir, input2_dir, output_dir, name):
+def html_compare(input1_dir, input2_dir, output_dir, name, sample=-1):
     entry_path1 = output_dir + "/" + input1_dir + "/combined/" + name + ".csv"
     entry_path2 = output_dir + "/" + input2_dir + "/combined/" + name + ".csv"
     output_root = "outputhtml/" + name
     os.makedirs(output_root, exist_ok=True)
-    for log_entry_pair in csv_reader.get_log_entry_pairs(entry_path1, entry_path2, ignore_validity_check=True):
+
+    log_entry_pairs = csv_reader.get_log_entry_pairs(entry_path1, entry_path2, ignore_validity_check=True)
+
+    def random_change_sample(entries_gen):
+        entries_list = [e for e in entries_gen if e[name + "-1"] != e[name + "-2"]]
+        random.seed(42)  # To allow reproduction of results
+        return random.sample(entries_list, sample)
+
+    if sample >= 1:
+        log_entry_pairs = random_change_sample(log_entry_pairs)
+
+    for log_entry_pair in log_entry_pairs:
         value1_str_list, value2_str_list = log_entry_pair[name + "-1"], log_entry_pair[name + "-2"]
         value1_list, value2_list = ast.literal_eval(value1_str_list), ast.literal_eval(value2_str_list)
         if value1_list != value2_list:
@@ -67,4 +79,4 @@ def html_compare(input1_dir, input2_dir, output_dir, name):
 
 
 # print(compute_change("testminiday", "testminiday2", "outputmini", "text"))
-# html_compare("testminiday", "testminiday2", "outputmini", "text")
+html_compare("testminiday", "testminiday2", "outputmini", "text", sample=10)
