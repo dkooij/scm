89c0a32f676c9a45301148769edb622a5396f298
Daan Kooij <d.kooij-1@student.utwente.nl>
2021-12-01 14:26:52 +0100

    Update crawl data usage example

--- analyzer/hadoop_decompress.py
+++ analyzer/hadoop_decompress.py
@@ -1,7 +1,7 @@
 """
 Module used to demonstrate how to read compressed Pickle crawls.
 Author: Daan Kooij
-Last modified: November 23rd, 2021
+Last modified: December 1st, 2021
 """
 
 import hashlib
@@ -11,7 +11,7 @@ import zlib
 
 
 # Initialize Spark and SparkSQL context.
-sc = SparkContext(appName="SCM-DECOMPRESS-S1839047")
+sc = SparkContext(appName="SCM-DECOMPRESS-CRAWLS-EXAMPLE")
 sc.setLogLevel("ERROR")
 spark = SparkSession.builder.getOrCreate()
 
@@ -22,9 +22,8 @@ def decompress(log_entry):
     return log_entry
 
 
-rdd = sc.pickleFile("/user/s1839047/crawls_compressed/20210613")
-print(list(rdd.first()[1].keys()))
-# print(rdd.mapValues(decompress).first())
-for k, v in rdd.mapValues(decompress).take(10):
+rdd = sc.pickleFile("/user/s1839047/crawls/data/20210612.pickle")
+print(list(rdd.first()[1].keys()))  # Print the keys of the data entries
+for k, v in rdd.mapValues(decompress).take(10):  # Print MD5 hashes of 10 pages
     print(k, hashlib.md5(str(v["Binary data"]).encode("utf-8")).hexdigest())
-print(rdd.count())
+print(rdd.count())  # Print the number of data entries
