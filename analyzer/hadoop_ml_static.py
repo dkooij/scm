5d43dc06822c5a3bbff585469343155bda8340a3
Daan Kooij <d.kooij-1@student.utwente.nl>
2022-01-11 14:22:41 +0100

    Add SVM, NB, DT, and RF classifiers
    Note that the DT and RF classifiers do not yet take into account the distribution of the data over the target values

--- analyzer/hadoop_ml_static.py
+++ analyzer/hadoop_ml_static.py
@@ -1,17 +1,17 @@
 """
 Train ML models to predict page text changes using static features.
 Author: Daan Kooij
-Last modified: December 20th, 2021
+Last modified: January 11th, 2021
 """
 
 from pyspark import SparkContext
-from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.classification import DecisionTreeClassifier, LinearSVC, LogisticRegression, NaiveBayes, RandomForestClassifier
 from pyspark.ml.linalg import DenseVector
 from pyspark.mllib.evaluation import MulticlassMetrics
 from pyspark.sql import Row, SparkSession
 
 
-INPUT_PATH = "extracted/static-training-pairs-combined.csv"
+INPUT_PATH = "extracted/static-training-pairs-combined-sample.csv"
 
 
 # Initialize Spark and SparkSQL context.
@@ -30,9 +30,27 @@ df = df.rdd.map(lambda row: Row(**{"features": row["features"],
                                    "weight": weight_zero if row["target"] == 0 else weight_one})).toDF()
 (data_train, data_test) = df.randomSplit([0.8, 0.2], seed=42)
 
-lr = LogisticRegression(labelCol="target", featuresCol="features", weightCol="weight", threshold=0.5)
-model = lr.fit(data_train)
+
+model_type = "lr"
+if model_type == "lr":
+    lr = LogisticRegression(labelCol="target", featuresCol="features", weightCol="weight")
+    model = lr.fit(data_train)
+elif model_type == "svm":
+    svm = LinearSVC(labelCol="target", featuresCol="features", weightCol="weight")
+    model = svm.fit(data_train)
+elif model_type == "nb":
+    nb = NaiveBayes(labelCol="target", featuresCol="features", weightCol="weight")
+    model = nb.fit(data_train)
+elif model_type == "dt":
+    # TODO: weights
+    dt = DecisionTreeClassifier(labelCol="target", featuresCol="features")
+    model = dt.fit(data_train)
+else:  # elif model_type == "rf":
+    # TODO: weights
+    rf = RandomForestClassifier(labelCol="target", featuresCol="features")
+    model = rf.fit(data_train)
 predictions = model.transform(data_test)
+
 
 predictions_rdd = predictions.select("target", "prediction").rdd.map(
     lambda row: (float(row["target"]), float(row["prediction"])))
