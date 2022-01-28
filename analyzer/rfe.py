bf398a72c6d1f6896d9c05a5c8bb23db363b2091
Daan Kooij <d.kooij-1@student.utwente.nl>
2022-01-28 12:51:46 +0100

    Print RFE leftover features

--- analyzer/rfe.py
+++ analyzer/rfe.py
@@ -1,36 +1,40 @@
 """
 Investigate how model performance decreases as certain features are dropped.
 Author: Daan Kooij
-Last modified: January 26th, 2022
+Last modified: January 28th, 2022
 """
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import RFE
 
 
-features = []
+feature_values = []
 targets = []
 
 with open("inputmisc/static-training-pairs-single-day.csv") as file:
     for line in file:
         parts = line.split(",")
-        features.append([int(x) for x in parts[1:10]])
+        feature_values.append([int(x) for x in parts[1:10]])
         targets.append(int(parts[10]))
-
 
 rf = RandomForestClassifier(n_estimators=20, random_state=42)
 
-num_features = len(features[0])
-features_cut = [v for v in features]
+feature_names = ["Email links", "External outlinks", "Images", "Internal outlinks",
+                  "Metas", "Page text", "Scripts", "Tables", "Tags"]
+feature_values_cut, feature_names_cut = feature_values, feature_names
+num_features = len(feature_values_cut[0])
+
 while num_features >= 1:
-    rf.fit(features_cut, targets)
-    print(str(num_features) + " features: " + str(rf.score(features_cut, targets)))
+    rf.fit(feature_values_cut, targets)
+    print(str(num_features) + " features score: " + str(round(rf.score(feature_values_cut, targets), 4)) +
+          " (selected features: " + str(feature_names_cut) + ")")
 
     if num_features > 1:
         rfe = RFE(rf, n_features_to_select=num_features - 1)
-        rfe.fit(features_cut, targets)
+        rfe.fit(feature_values_cut, targets)
         ranking = rfe.ranking_
-        features_cut = [[x[0] for x in zip(v, ranking) if x[1] == 1] for v in features_cut]
-        num_features = len(features_cut[0])
+        feature_values_cut = [[x[0] for x in zip(v, ranking) if x[1] == 1] for v in feature_values_cut]
+        feature_names_cut = [x[0] for x in zip(feature_names_cut, ranking) if x[1] == 1]
+        num_features = len(feature_values_cut[0])
     else:
         break
