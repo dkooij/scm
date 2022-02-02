59bfc7001e4c6635999aad69e980718a4dfd9d1f
Daan Kooij <d.kooij-1@student.utwente.nl>
2022-02-02 11:23:31 +0100

    Add labels to heatmap plots and optimize appearance

--- analyzer/graph_heatmap.py
+++ analyzer/graph_heatmap.py
@@ -1,10 +1,12 @@
 """
 Visualize how predictions by ML models are made by plotting predictions for all values in a grid.
 Author: Daan Kooij
-Last modified: February 1st, 2022
+Last modified: February 2nd, 2022
 """
 
 import matplotlib.pyplot as plt
+
+import global_vars
 
 
 INPUT_DIR = "inputmisc/grid"
@@ -39,7 +41,7 @@ def draw_single_heatmap(model_name, feature1, feature2, figure=None):
 
 def draw_heatmaps(model_name, feature_pairs, rows, columns, figure=None):
     if figure is None:
-        fig = plt.figure(figsize=(9.6, 4.8))
+        fig = plt.figure(figsize=(3.2 * columns, 2.4 * rows))
     else:
         fig = figure
 
@@ -48,23 +50,27 @@ def draw_heatmaps(model_name, feature_pairs, rows, columns, figure=None):
         plot_index += 1
         if x != y:
             ax = fig.add_subplot(rows, columns, plot_index)
+            plt.xlabel(global_vars.FEATURE_MAP[x] + " →")
+            plt.ylabel(global_vars.FEATURE_MAP[y] + " →")
             if figure is not None:
-                ax.axis("off")
+                ax.get_xaxis().set_ticks([])
+                ax.get_yaxis().set_ticks([])
             draw_single_heatmap(model_name, x, y, figure=ax)
 
+    fig.suptitle("Visualisation of decisions made by " + model_name + " model")
     fig.tight_layout()
     if figure is None:
         fig.savefig("figures/ml-static/heatmaps_important.png", dpi=400)
 
 
 def draw_all_heatmaps(model_name):
-    fig = plt.figure()
+    fig = plt.figure(figsize=(19.2, 14.4))
     feature_pairs = [(x, y) for x in range(9) for y in range(9)]
     draw_heatmaps(model_name, feature_pairs, 9, 9, figure=fig)
     fig.tight_layout()
-    fig.savefig("figures/ml-static/heatmaps_all.png", dpi=900)
+    fig.savefig("figures/ml-static/heatmaps_all.png", dpi=300)
 
 
 # draw_single_heatmap("rf", 5, 8)
-draw_heatmaps("rf", [(8, 5), (8, 6), (8, 2), (5, 6), (5, 2), (6, 2)], 2, 3)  # TODO: need axis labels
+draw_heatmaps("rf", [(8, 5), (8, 6), (8, 2), (5, 6), (5, 2), (6, 2)], 2, 3)
 draw_all_heatmaps("rf")
