c66842d47c4f02cee275caa352440f81dcc07a7c
Daan Kooij <d.kooij-1@student.utwente.nl>
2022-02-02 12:17:05 +0100

    Add functionality to plot (scatter) sample of datapoints in heatmap

--- analyzer/graph_heatmap.py
+++ analyzer/graph_heatmap.py
@@ -5,11 +5,36 @@ Last modified: February 2nd, 2022
 """
 
 import matplotlib.pyplot as plt
+import random
 
 import global_vars
 
 
 INPUT_DIR = "inputmisc/grid"
+
+
+def load_training_pair_data(sample_size):
+    features0, features1 = [], []
+
+    with open("inputmisc/static-training-pairs-combined-2-sample.csv") as file:
+        for line in file:
+            parts = line.split(",")
+            features = [int(x) for x in parts[1:10]]
+            target = int(parts[10])
+            if target == 0:
+                features0.append(features)
+            else:
+                features1.append(features)
+
+    random.seed(42)
+    features0_sample = random.sample(features0, int(sample_size / 2))
+    features1_sample = random.sample(features1, int(sample_size / 2))
+
+    data_sample = []
+    for f0, f1 in zip(features0_sample, features1_sample):
+        data_sample.append((f0, 0))
+        data_sample.append((f1, 1))
+    return data_sample
 
 
 def draw_single_heatmap(model_name, feature1, feature2, figure=None):
@@ -37,9 +62,11 @@ def draw_single_heatmap(model_name, feature1, feature2, figure=None):
         fig = figure
 
     fig.contourf(xss, yss, zss, cmap=plt.cm.RdBu, vmin=0.25, vmax=0.75)
+    fig.set_xlim(left=xss[0][0], right=xss[-1][-1])
+    fig.set_ylim(bottom=yss[0][0], top=yss[-1][-1])
 
 
-def draw_heatmaps(model_name, feature_pairs, rows, columns, figure=None):
+def draw_heatmaps(model_name, feature_pairs, rows, columns, tpd=None, figure=None):
     if figure is None:
         fig = plt.figure(figsize=(3.2 * columns, 2.4 * rows))
     else:
@@ -56,6 +83,11 @@ def draw_heatmaps(model_name, feature_pairs, rows, columns, figure=None):
                 ax.get_xaxis().set_ticks([])
                 ax.get_yaxis().set_ticks([])
             draw_single_heatmap(model_name, x, y, figure=ax)
+            if tpd is not None:
+                xs = [t[0][x] for t in tpd]
+                ys = [t[0][y] for t in tpd]
+                cs = [t[1] for t in tpd]
+                ax.scatter(xs, ys, c=cs, s=0.4, cmap=plt.cm.RdBu, vmin=0, vmax=1)
 
     fig.suptitle("Visualisation of decisions made by " + model_name + " model")
     fig.tight_layout()
@@ -63,14 +95,15 @@ def draw_heatmaps(model_name, feature_pairs, rows, columns, figure=None):
         fig.savefig("figures/ml-static/heatmaps_important.png", dpi=400)
 
 
-def draw_all_heatmaps(model_name):
+def draw_all_heatmaps(model_name, tpd=None):
     fig = plt.figure(figsize=(19.2, 14.4))
     feature_pairs = [(x, y) for x in range(9) for y in range(9)]
-    draw_heatmaps(model_name, feature_pairs, 9, 9, figure=fig)
+    draw_heatmaps(model_name, feature_pairs, 9, 9, tpd=tpd, figure=fig)
     fig.tight_layout()
     fig.savefig("figures/ml-static/heatmaps_all.png", dpi=300)
 
 
+# _tpd = load_training_pair_data(1000)
 # draw_single_heatmap("rf", 5, 8)
 draw_heatmaps("rf", [(8, 5), (8, 6), (8, 2), (5, 6), (5, 2), (6, 2)], 2, 3)
 draw_all_heatmaps("rf")
