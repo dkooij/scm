c9959fc6423fd03b400907ec8a32e873787b73df
Daan Kooij <d.kooij-1@student.utwente.nl>
2022-02-09 16:20:38 +0100

    Adapt heatmap grid to models trained using feature pairs

--- analyzer/hadoop_ml_heatmap.py
+++ analyzer/hadoop_ml_heatmap.py
@@ -1,7 +1,7 @@
 """
 Visualize how predictions by ML models are made by making predictions for all values in a grid.
 Author: Daan Kooij
-Last modified: February 4th, 2022
+Last modified: February 9th, 2022
 """
 
 import numpy as np
@@ -17,7 +17,7 @@ spark = SparkSession.builder.getOrCreate()
 
 
 def load_model(model_type, model_name):
-    model_path = "models/" + model_name + ".model"
+    model_path = "models/projections/" + model_name + ".model"
     if model_type == "rf":
         return RandomForestClassificationModel.load(model_path)
     elif model_type == "lr":
@@ -25,39 +25,46 @@ def load_model(model_type, model_name):
 
 
 def create_feature_grid(feature_statistics, feature1, feature2, resolution):
-    values = [t[2] for t in feature_statistics]
+    values = [0, 0]
     vectors = []
 
     for x in sorted(list(set(np.linspace(feature_statistics[feature1][0], feature_statistics[feature1][1],
                                          resolution, dtype=int)))):
-        values[feature1] = x
+        values[0] = x
         for y in sorted(list(set(np.linspace(feature_statistics[feature2][0], feature_statistics[feature2][1],
                                              resolution, dtype=int)))):
-            values[feature2] = y
+            values[1] = y
             vectors.append(DenseVector(values))
 
     return spark.createDataFrame(data=[[vector] for vector in vectors], schema=["features"])
 
 
 def create_heatmap(model_name, model, feature_statistics, feature1, feature2, resolution):
-    feature_grid = create_feature_grid(feature_statistics, feature1, feature2, resolution)
+    if feature1 < feature2:
+        feature_grid = create_feature_grid(feature_statistics, feature1, feature2, resolution)
+    else:
+        feature_grid = create_feature_grid(feature_statistics, feature2, feature1, resolution)
     predictions = model.transform(feature_grid)
 
     def to_csv_line(rdd_row):
         features, probability = rdd_row["features"], rdd_row["probability"][0]
-        return ",".join([str(int(features[feature1])), str(int(features[feature2])), str(probability)])
+        if feature1 < feature2:
+            return ",".join([str(int(features[0])), str(int(features[1])), str(probability)])
+        else:
+            return ",".join([str(int(features[1])), str(int(features[0])), str(probability)])
 
-    output_path = "predicted/grid/" + model_name + "-" + str(feature1) + "-" + str(feature2) + ".csv"
+    output_path = "predicted/grid2/" + model_name + "-" + str(feature1) + "-" + str(feature2) + ".csv"
     predictions.rdd.coalesce(1).map(to_csv_line).saveAsTextFile(output_path)
 
 
 def create_heatmaps(model_type, model_name, feature_statistics, feature_pairs, resolution):
-    model = load_model(model_type, model_name)
     for feature1, feature2 in feature_pairs:
+        full_model_name = model_name + "-" + str(min(feature1, feature2)) + "-" + str(max(feature1, feature2))
+        model = load_model(model_type, full_model_name)
         create_heatmap(model_name, model, feature_statistics, feature1, feature2, resolution)
 
 
-_model_type, _model_name = "lr", "lr"
+_model_type, _model_name = "rf", "rf"
 _feature_statistics = [(0, 3, 0), (0, 76, 5), (0, 86, 6), (0, 339, 41), (0, 37, 9),
                        (2, 792, 120), (0, 65, 14), (0, 8, 0), (6, 2186, 368)]
 _feature_pairs = [(x, y) for x in range(9) for y in range(9) if x != y]
