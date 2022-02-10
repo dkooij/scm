6373a2896d0745fcf095b17a46334410579be367
Daan Kooij <d.kooij-1@student.utwente.nl>
2022-02-10 17:32:04 +0100

    Increase clarity of classification plots (heatmaps)

--- analyzer/graph_heatmap.py
+++ analyzer/graph_heatmap.py
@@ -1,16 +1,17 @@
 """
 Visualize how predictions by ML models are made by plotting predictions for all values in a grid.
 Author: Daan Kooij
-Last modified: February 4th, 2022
+Last modified: February 10th, 2022
 """
 
+from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import random
 
 import global_vars
 
 
-INPUT_DIR = "inputmisc/grid"
+INPUT_DIR = "inputmisc/grid2"
 
 
 def load_training_pair_data(sample_size):
@@ -37,7 +38,7 @@ def load_training_pair_data(sample_size):
     return data_sample
 
 
-def draw_single_heatmap(model_name, feature1, feature2, figure=None):
+def draw_single_heatmap(model_name, feature1, feature2, feature_statistics, figure=None):
     xss, yss, zss = [], [], []
 
     with open(INPUT_DIR + "/" + model_name + "-" + str(feature1) + "-" + str(feature2) + ".csv/part-00000") as file:
@@ -61,12 +62,20 @@ def draw_single_heatmap(model_name, feature1, feature2, figure=None):
     else:
         fig = figure
 
-    fig.contourf(xss, yss, zss, cmap=plt.cm.RdBu, vmin=0.25, vmax=0.75)
+    fig.contourf(xss, yss, zss, cmap=plt.cm.RdBu, vmin=0.0, vmax=1.0)
     fig.set_xlim(left=xss[0][0], right=xss[-1][-1])
     fig.set_ylim(bottom=yss[0][0], top=yss[-1][-1])
 
+    median_f1, median_f2 = feature_statistics[feature1][2], feature_statistics[feature2][2]
+    fig.set_xscale("symlog", linthresh=max(median_f1, 1))
+    fig.set_yscale("symlog", linthresh=max(median_f2, 1))
+    fig.plot([median_f1, median_f1], [feature_statistics[feature2][0], feature_statistics[feature2][1]],
+             color="black", linewidth=3.0, linestyle=(0, (3, 2)))
+    fig.plot([feature_statistics[feature1][0], feature_statistics[feature1][1]], [median_f2, median_f2],
+             color="black", linewidth=3.0, linestyle=(0, (3, 2)))
 
-def draw_heatmaps(model_name, feature_pairs, rows, columns, tpd=None, figure=None):
+
+def draw_heatmaps(model_name, feature_pairs, rows, columns, feature_statistics, tpd=None, figure=None):
     if figure is None:
         fig = plt.figure(figsize=(3.2 * columns, 2.4 * rows))
     else:
@@ -82,12 +91,12 @@ def draw_heatmaps(model_name, feature_pairs, rows, columns, tpd=None, figure=Non
             if figure is not None:
                 ax.get_xaxis().set_ticks([])
                 ax.get_yaxis().set_ticks([])
-            draw_single_heatmap(model_name, x, y, figure=ax)
+            draw_single_heatmap(model_name, x, y, feature_statistics, figure=ax)
             if tpd is not None:
                 xs = [t[0][x] for t in tpd]
                 ys = [t[0][y] for t in tpd]
-                cs = [1 - t[1] for t in tpd]  # One minus the target value because seismic colormap is opposite.
-                ax.scatter(xs, ys, c=cs, s=0.4, cmap=plt.cm.seismic, vmin=0, vmax=1)
+                cs = [t[1] for t in tpd]  # One minus the target value because colormap is opposite.
+                ax.scatter(xs, ys, c=cs, s=2.0, cmap=ListedColormap(["red", "blue"]), vmin=0, vmax=1, zorder=10)
 
     fig.suptitle("Visualisation of decisions made by " + model_name + " model")
     fig.tight_layout()
@@ -95,15 +104,17 @@ def draw_heatmaps(model_name, feature_pairs, rows, columns, tpd=None, figure=Non
         fig.savefig("figures/ml-static/" + model_name + "-heatmaps-important.png", dpi=400)
 
 
-def draw_all_heatmaps(model_name, tpd=None):
+def draw_all_heatmaps(model_name, feature_statistics, tpd=None):
     fig = plt.figure(figsize=(19.2, 14.4))
     feature_pairs = [(x, y) for x in range(9) for y in range(9)]
-    draw_heatmaps(model_name, feature_pairs, 9, 9, tpd=tpd, figure=fig)
+    draw_heatmaps(model_name, feature_pairs, 9, 9, feature_statistics, tpd=tpd, figure=fig)
     fig.tight_layout()
     fig.savefig("figures/ml-static/" + model_name + "-heatmaps-all.png", dpi=300)
 
 
-# _tpd = load_training_pair_data(1000)
-# draw_single_heatmap("rf", 5, 8)
-draw_heatmaps("rf", [(8, 5), (8, 6), (8, 2), (5, 6), (5, 2), (6, 2)], 2, 3)
-draw_all_heatmaps("rf")
+_tpd = load_training_pair_data(1000)
+_fs = [(0, 3, 0), (0, 76, 5), (0, 86, 6), (0, 339, 41), (0, 37, 9),
+       (2, 792, 120), (0, 65, 14), (0, 8, 0), (6, 2186, 368)]
+# draw_single_heatmap("rf", 5, 8, _fs)
+draw_heatmaps("rf", [(8, 5), (8, 6), (8, 3), (5, 6), (5, 3), (6, 3)], 2, 3, _fs, tpd=_tpd)
+draw_all_heatmaps("rf", _fs)
